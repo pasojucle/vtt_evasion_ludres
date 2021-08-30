fe2363cd34e7db2a7709be5921701b2297b43bef
pasojucle <pasojucle@gmail.com>
2021-08-30 21:39:10 +0200

    Pouvoir modifie le mot de passe #24

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -162,7 +162,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('contact@vttevasionludres.fr', 'mail bot'))
             ->to($user->getFirstIdentity()->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Réinitialiser votre mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
