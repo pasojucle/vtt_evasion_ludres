3601cb9cad8bb281feb3181cfa3b535ef9f61453
pasojucle <pasojucle@gmail.com>
2021-09-02 19:37:11 +0200

    ajouter groupe "en attente d'évaluation" pour les nouveaux #43

--- migrations/Version20210902162540.php
+++ migrations/Version20210902162540.php
@@ -26,6 +26,17 @@ final class Version20210902162540 extends AbstractMigration
         $level = ['id' => $maxLevelId];
         $this->addSql('INSERT INTO `level`(id, `title`, `content`, `order_by`, `type`, `is_protected`) VALUES (:id, "En cours d\'évaluation","",0,1,1)', $level);
         $this->addSql('UPDATE `user` AS u INNER JOIN licence AS l ON l.user_id = u.id SET u.level_id= :id WHERE l.final = 0', $level);
+        $events = $this->connection->fetchAllAssociative('SELECT `id` FROM `event` WHERE `type`= 2');
+        if (!empty($events)) {
+            foreach($events as $event) {
+                $cluster = [
+                    'eventId' => $event['id'],
+                    'title' => "En cours d\'évaluation",
+                    'levelId' => $maxLevelId,
+                ];
+                $this->addSql('INSERT INTO `cluster`(`event_id`, `title`, `level_id`) VALUES (:eventId, :title, :levelId)', $cluster);
+            }
+        }
     }
 
     public function down(Schema $schema): void
