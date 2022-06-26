73d84228851a6ceaa07dc9a5b7b27f384b13b4b4
pasojucle <pasojucle@free.fr>
2022-06-26 18:39:18 +0200

    Pouvoir ajouter et modifier les images de fond #71

--- public/js/media_position.js
+++ public/js/media_position.js
@@ -1,116 +1,6 @@
-(function( $ ){
-    $.fn.mediaPosition = function(options) {
-        var defaults = {
-            displayWidth: 584,
-            displayHeight: 225,
-        };
 
-        options = $.extend(defaults, options);
-
-        return this.each(function() {
-            var canvas, context;
-            var canvasImage = new Image();
-            var isDraggable = false;
-
-            var startY = 0;
-            var currentY = 0;
-            var ratioImg = 1;
-            var ratioCanvas = 1;
-            var zone = {};
-            var imagePosition;
-
-            this.initialize = function() {
-                canvas = this;
-                context = canvas.getContext("2d");
-                canvasImage.src = $(canvas).data('src');
-                const form = $(canvas).closest("form")[0];
-                const prefix = form.name;
-
-                console.log(this);
-
-                canvasImage.onload = function() {
-                    console.log('onload');
-                    ratioImg = canvas.width / canvasImage.width;
-                    ratioCanvas = canvas.width / options.displayWidth;
-                    canvas.height = canvasImage.height * ratioImg;
-                    currentY = canvas.height;
-                    zone.positionX = 0;
-                    zone.width = options.displayWidth * ratioCanvas;
-                    zone.height = options.displayHeight * ratioCanvas;
-                    imagePosition = $('#'+prefix+'_mediaPositionY');
-                    if (!imagePosition.val() || (imagePosition.val() * ratioCanvas +zone.height) > currentY) {
-                        zone.positionY = (currentY-zone.height)/2;
-                        imagePosition.val(zone.positionY / ratioCanvas) ;
-                    } else {
-                        zone.positionY = imagePosition.val() * ratioCanvas;
-                    }
-                    run();
-                };
-                const fileInput = document.getElementById(prefix+'_backgroundFile');
-                fileInput.onchange = function() {
-                    console.log(canvasImage);
-                    canvasImage.src = URL.createObjectURL(fileInput.files[0]);
-                };
-                return this;
-            };
-
-
-            var run = function() {
-                mouseEvents();
-                setInterval(function() {
-                    resetCanvas();
-                    drawZone();
-                }, 1000/30);
-            };
-            var resetCanvas = function() {
-                context.drawImage(canvasImage, 0, 0, canvasImage.width, canvasImage.height, 0, 0, canvas.width, canvas.height);
-            };
-            var mouseEvents = function() {
-                canvas.onmousedown = function(e) {
-                    var mouseY = e.pageY - $(this).offset().top;
-                    startY = mouseY - zone.positionY ;
-
-                    if (mouseY >= zone.positionY &&
-                        mouseY <= (zone.positionY + zone.height)) {
-                        isDraggable = true;
-                        document.body.style.cursor = "move";
-                    }
-                };
-                canvas.onmousemove = function(e) {
-                    if (isDraggable) {
-                        currentY = e.pageY - $(this).offset().top;
-                        zone.positionY = currentY - startY;
-
-                        if (zone.positionY <= 0 || (zone.positionY + zone.height) >= canvas.height) {
-                            if (zone.positionY < 0) {
-                                zone.positionY = 0;
-                            }
-                            if ((zone.positionY + zone.height) > canvas.height) {
-                                zone.positionY = canvas.height - zone.height;
-                            }
-                        }
-                    }
-                };
-                document.onmouseup = function(e) {
-                    isDraggable = false;
-                    document.body.style.cursor = "auto";
-                    imagePosition.val(zone.positionY / ratioCanvas);
-                };
-            };
-            var drawZone = function() {
-                context.fillStyle = 'rgba(0, 0, 0, 0.4)';
-                context.fillRect(zone.positionX,0, zone.width, zone.positionY);
-                context.fillRect(zone.positionX,zone.positionY + zone.height, zone.width, canvas.height - zone.positionY - zone.height);
-            };
-
-            return this.initialize();
-        });
-    };
-})( jQuery );
 $(function() {
-    // $('.media-position').mediaPosition();
     document.querySelectorAll('.media-position').forEach(canvas => {
-        console.log(canvas);
         new MediaPosition(canvas);
     });;
 });
@@ -120,106 +10,129 @@ $(function() {
 class MediaPosition {
     canvas
     context;
+    reader;
     image = new Image();
-    orientation;
     ratio = 1;
 
     startX = 0;
     startY = 0;
     currentY = 0;
 
-    zoneLandscape;
-    zoneSquare;
+    zones = [];
     imagePosition;
     display = {};
     gap = 10;
-    
-    prefix;
+    fileInput;
+
     constructor(canvas) {
-        this.display.width = 350;
+        this.display = {'width': 350, 'height': 0};
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
+
+        this.reader = new FileReader()
 
         this.image.src = canvas.dataset.src;
         const form = $(canvas).closest("form")[0];
         let prefix = form.name;
         this.image.onload = this.initialize();
-        const fileInput = document.getElementById(prefix+'_backgroundFile');
         const self = this;
-        fileInput.onchange = function() {
-            self.canvasImage.src = URL.createObjectURL(fileInput.files[0]);
-        };
+        
+        this.fileInput = document.getElementById(prefix+'_backgroundFile');
+        
+        this.fileInput.onchange = (e => this.changeImage());
     }
-    initialize() {
-        console.log('onload');
+    initialize(e) {
+
+
+        console.log('onLoad', this.display.width, this.image.width);
+        console.log('onLoad', this.display.width, this.image);
+        this.zones = [
+            {'name': 'landscape', 'outputWidth': 1920, 'outputHeight': 1080},
+            {'name': 'square', 'outputWidth': 850, 'outputHeight': 850},
+        ];
 
         this.ratio = this.display.width / this.image.width;
-        this.display.height = this.image.height * this.ratio;
+        this.display['height'] = this.image.height * this.ratio;
+
+        Object.entries(this.zones).forEach(([key, zone]) => 
+            this.zones[key]['zone'] = new Zone(this.display, key, zone)
+        );
+
         if (this.image.width > this.image.height) {
             this.canvas.width = this.image.width * this.ratio;
             this.canvas.height = this.image.height * this.ratio *2;
-            this.display.landscapeX = 0;
-            this.display.landscapeY = 0;
-            this.display.squareX = 0;
-            this.display.squareY = this.display.height + this.gap;
+            Object.entries(this.zones).forEach(([key, zone]) => zone.zone.setOrigin(0, (this.display.height + this.gap) * key));
         } else {
             this.canvas.width = this.image.width * this.ratio *2;
             this.canvas.height = this.image.height * this.ratio;
-            this.display.landscapeX = 0;
-            this.display.landscapeY = 0;
-            this.display.squareX = this.display.width + this.gap;
-            this.display.squareY = 0;
+            Object.entries(this.zones).forEach(([key, zone]) => zone.zone.setOrigin((this.display.width + this.gap) * key, 0));
         }
-        
-        this.zoneLandscape = new zone(this, 'landscape', 1520, 1080);
-        this.zoneSquare = new zone(this, 'square', 850, 850);
+
         this.imagePosition = $('#'+this.prefix+'_mediaPositionY');
 
         this.run();
     }
-
     run() {
         console.log('this', this);
         this.mouseEvents();
         self = this;
-        setInterval(function() {
-            self.resetCanvas();
-            self.drawZone(self.zoneLandscape);
-            self.drawZone(self.zoneSquare);
+        this.interval = setInterval(function() {
+            self.drawImage();
+            Object.entries(self.zones).forEach(([key, zone]) => self.drawZone(zone.zone));
         }, 1000/30);
     }
-    resetCanvas() {
-        this.context.drawImage(this.image, 0, 0, this.image.width, this.image.height, this.display.landscapeX, this.display.landscapeY, this.display.width, this.display.height);
-        this.context.drawImage(this.image, 0, 0, this.image.width, this.image.height, this.display.squareX, this.display.squareY, this.display.width, this.display.height);
+    drawImage() {
+        Object.entries(this.zones).forEach(([key, zone]) => 
+            this.context.drawImage(this.image, 0, 0, this.image.width, this.image.height, zone.zone.origin.x, zone.zone.origin.y, this.display.width, this.display.height)
+        );
     }
     mouseEvents() {
         self = this;
         this.canvas.onmousedown = function(e) {
             var mouseY = e.pageY - e.target.offsetTop;
             var mouseX = e.pageX - e.target.offsetLeft;
-            // self.startX = mouseX - self.zoneLandscape.positionX ;
-            // self.startY = mouseY - self.zoneLandscape.positionY ;
-            self.zoneLandscape.setDraggable(mouseX, mouseY);
+
+            Object.entries(self.zones).forEach(([key, zone]) => zone.zone.StartDraggable(mouseX, mouseY));
 
         };
         this.canvas.onmousemove = function(e) {
             self.currentY = e.pageY - e.target.offsetTop;
-            self.zoneLandscape.move(self);
+            Object.entries(self.zones).forEach(([key, zone]) => zone.zone.move(self));
         };
         document.onmouseup = function(e) {
-            self.zoneLandscape.setPositions();
+            Object.entries(self.zones).forEach(([key, zone]) => zone.zone.endDraggable());
+
         };
     };
     drawZone(zone) {
-        this.context.fillStyle = 'rgba(0, 0, 0, 0.6)';
-        this.context.fillRect(0,0, this.display.width, zone.positionY);
-        this.context.fillRect(0,0, zone.positionX, this.display.height);
-        this.context.fillRect(0,zone.positionY+zone.height, this.display.width, this.display.height-zone.height-zone.positionY);
-        this.context.fillRect(zone.positionX+zone.width,0, this.display.width-zone.width-zone.positionX, this.display.height);
-    };
+        this.context.fillStyle = 'rgba(0, 0, 0, 0.7)';
+        //top
+         this.context.fillRect(zone.origin.x,zone.origin.y, this.display.width, zone.positionY);
+        //bottom
+        this.context.fillRect(zone.origin.x,zone.positionY+zone.height, this.display.width, this.display.height-zone.height-zone.positionY);
+        //left
+        this.context.fillRect(zone.origin.x,zone.origin.y, zone.positionX, this.display.height);
+        //rigth
+        this.context.fillRect(zone.positionX+zone.width,zone.origin.y, this.display.width-zone.width-zone.positionX, this.display.height);
+    }
+    changeImage() {
+        this.image = new Image();
+        this.image.src = URL.createObjectURL(this.fileInput.files[0]);
+        var reader = new FileReader()
+        reader.onload = async (e) => {
+            let image = new Image()
+            image.src = e.target.result
+            await image.decode()
+                    self.initialize();
+                    Object.entries(self.zones).forEach(([key, zone]) => 
+                    zone.zone.defaultPositions(self.display)
+                );
+            }
+        reader.readAsDataURL(this.fileInput.files[0]);
+    }
 }
 
-class zone {
+class Zone {
 
     isDraggable = false;
     width;
@@ -229,42 +142,42 @@ class zone {
     positionY;
     inputselector;
     imagePositions;
+    origin = {};
+    name;
 
     startY = 0;
     currentY = 0;
-    constructor(canvas, name, outputWidth, outputHeight) {
-        if(outputWidth / outputHeight < canvas.display.width / canvas.display.height) {
-            this.ratio = canvas.display.height / outputHeight;
+    constructor(display, key, zone) {
+        this.name = zone.name;
+        if(zone.outputWidth / zone.outputHeight <  display.width / display.height) {
+            this.ratio =  display.height / zone.outputHeight;
         } else {
-            this.ratio = canvas.display.width / outputWidth;
+            this.ratio =  display.width / zone.outputWidth;
         }
-        
-        this.inputselector = document.querySelector('#background_' + name + 'Position');
-        console.log('inputselector', this.inputselector);
+        this.inputselector = document.querySelector('#background_' + zone.name + 'Position');
         this.imagePositions =  (this.inputselector && this.inputselector.value !== '') ? JSON.parse(this.inputselector.value) : {'positionX': null, 'positionY': null};
-        console.log('imagePositions', this.imagePositions);
-        this.height = outputHeight * this.ratio;
-        this.width = outputWidth * this.ratio;
+        this.height = zone.outputHeight * this.ratio;
+        this.width = zone.outputWidth * this.ratio;
 
-        if (this.imagePositions.positionX || this.imagePositions.positionY) {
+        if (this.imagePositions.positionX !== null && this.imagePositions.positionY !== null) {
             this.positionX = this.imagePositions.positionX * this.ratio;
             this.positionY = this.imagePositions.positionY * this.ratio;
         } else {
-            
-            this.positionX = (canvas.display.width- this.width) / 2;
-            this.positionY = (canvas.display.height - this.height)/2;
+            this.defaultPositions(display);
         }
-        console.log(this);
     }
-    setDraggable(mouseX, mouseY) {
-        if (mouseY >= this.positionY
-            && mouseY <= (this.positionY + this.height)
-            && mouseX >= this.positionX
-            && mouseX <= (this.positionX + this.width)
+    StartDraggable(mouseX, mouseY) {
+        if (mouseY >= this.origin.y + this.positionY
+            && mouseY <= (this.origin.y + this.positionY + this.height)
+            && mouseX >= this.origin.x + this.positionX
+            && mouseX <= (this.origin.x + this.positionX + this.width)
         ) {
             this.isDraggable = true;
             document.body.style.cursor = "move";
         }
+    }
+    setOrigin(x, y) {
+        this.origin = {'x': x, 'y': y};
     }
     move(mediaPosition) {
         if (this.isDraggable) {
@@ -279,12 +192,20 @@ class zone {
             }
         }
     }
-    setPositions() {
+    endDraggable() {
         if (this.isDraggable) {
             this.isDraggable = false;
             document.body.style.cursor = "auto";
-            this.inputselector.value = JSON.stringify({'positionX': this.positionX, 'positionY': this.positionY});
-            console.log(this.inputselector.value);
+            this.setPositions();
         }
+    }
+    setPositions() {
+        this.inputselector.value = JSON.stringify({'positionX': this.positionX, 'positionY': this.positionY});
+        console.log(this.inputselector.value);
+    }
+    defaultPositions(display) {
+        this.positionX = ( display.width - this.width) / 2;
+        this.positionY = ( display.height  - this.height) / 2;
+        this.setPositions();
     }
 }
