edf39a99fb2928c9398edda4e47354e05c804bc2
pasojucle <pasojucle@free.fr>
2022-12-11 13:54:22 +0100

    Suppression fonction inutile #145

--- src/UseCase/Session/AddSession.php
+++ src/UseCase/Session/AddSession.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace App\UseCase\Session;
 
 use App\Entity\BikeRide;
-use App\Entity\Level;
-use App\Entity\Licence;
 use App\Entity\Respondent;
 use App\Entity\User;
 use DateTime;
@@ -51,24 +49,5 @@ class AddSession
             ;
             $this->entityManager->persist($respondent);
         }
-    }
-
-    private function confirmationRegistration(User $user, BikeRide $bikeRide): void
-    {
-        $this->userPresenter->present($user);
-        $this->bikeRidePresenter->present($bikeRide);
-        $user = $this->userPresenter->viewModel();
-        $bikeRide = $this->bikeRidePresenter->viewModel();
-        $content = (Licence::CATEGORY_MINOR === $user->seasonLicence->category)
-            ? 'EMAIL_ACKNOWLEDGE_SESSION_REGISTRATION_MINOR'
-            : (Level::TYPE_FRAME === $user->level->type ? 'EMAIL_ACKNOWLEDGE_SESSION_REGISTRATION_FRAME' : 'EMAIL_ACKNOWLEDGE_SESSION_REGISTRATION_ADULT');
-
-        $this->mailerService->sendMailToMember([
-            'name' => $user->member->name,
-            'firstName' => $user->member->firstName,
-            'email' => $user->mainEmail,
-            'subject' => 'Confirmation d\'inscription à une sortie',
-            'bikeRideTitleAndPeriod' => $bikeRide->title . ' du ' . $bikeRide->period,
-        ], $content);
     }
 }
