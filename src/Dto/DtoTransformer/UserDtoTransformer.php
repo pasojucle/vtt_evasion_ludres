d24ac094538b9494a13bf468a29e3a8265385eed
pasojucle <pasojucle@free.fr>
2023-09-03 16:26:12 +0200

    correction bug email de contact

--- src/Dto/DtoTransformer/UserDtoTransformer.php
+++ src/Dto/DtoTransformer/UserDtoTransformer.php
@@ -32,6 +32,7 @@ class UserDtoTransformer
         $identitiesByType = $this->identityDtoTransformer->fromEntities($user->getIdentities(), $changes);
 
         $userDto = new UserDto();
+        dump($user);
         $userDto->id = $user->getId();
         $userDto->licenceNumber = $user->getLicenceNumber();
         $userDto->member = (array_key_exists(Identity::TYPE_MEMBER, $identitiesByType)) ? $identitiesByType[Identity::TYPE_MEMBER] : null;
@@ -39,7 +40,6 @@ class UserDtoTransformer
         $userDto->secondKinship = (array_key_exists(Identity::TYPE_SECOND_CONTACT, $identitiesByType)) ? $identitiesByType[Identity::TYPE_SECOND_CONTACT] : null;
         
         $userDto->lastLicence = $this->getLastLicence($user, $changes);
-        // $userDto->seasonLicence = ($userDto->lastLicence->isSeasonLicence) ? $userDto->lastLicence : null;
 
         $userDto->health = $this->healthDtoTransformer->fromEntity($user->getHealth());
         $userDto->level = $this->levelDtoTransformer->fromEntity($user->getLevel());
@@ -72,7 +72,7 @@ class UserDtoTransformer
     public function getMainEmail(array $identitiesByType, int $category): ?string
     {
         if (!empty($identitiesByType)) {
-            $identity = (Licence::CATEGORY_MINOR === $category) ? $identitiesByType[Identity::TYPE_KINSHIP] : $identitiesByType[Identity::TYPE_MEMBER];
+            $identity = (Licence::CATEGORY_MINOR === $category && array_key_exists(Identity::TYPE_KINSHIP,$identitiesByType)) ? $identitiesByType[Identity::TYPE_KINSHIP] : $identitiesByType[Identity::TYPE_MEMBER];
             return $identity?->email;
         }
 
