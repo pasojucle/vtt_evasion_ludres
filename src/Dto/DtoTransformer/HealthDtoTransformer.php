3c010c2e4439d1a5c9c982555d6b6b8622f84978
pasojucle <pasojucle@free.fr>
2023-10-06 19:25:10 +0200

    Message erroné sur le bulletin d’inscription #196

--- src/Dto/DtoTransformer/HealthDtoTransformer.php
+++ src/Dto/DtoTransformer/HealthDtoTransformer.php
@@ -46,6 +46,10 @@ class HealthDtoTransformer
         if ($health->hasAtLeastOnePositveResponse()) {
             $message .= 'Vous avez répondu "oui" au moins à une réponse du questionnaire de santé. <br>';
         }
+        
+        if (!$health->hasAtLeastOnePositveResponse() && new DateTime() <= $medicalCertificateEndAt ) {
+            $message .= 'J\'atteste avoir répondu "NON" à toutes les questions du questionnaire de santé et ne pas fournir de nouveau certificat médical pour ma réinscription. <br>';
+        }
 
         if ($health->hasAtLeastOnePositveResponse() || null === $medicalCertificateEndAt || $medicalCertificateEndAt < new DateTime()) {
             $message .= 'Vous devez joindre un certificat médical daté DE MOINS DE 12 MOIS de non contre-indication à la pratique du VTT';
