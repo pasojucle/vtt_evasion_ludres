5dcb819dc3b52000fa7c1c45cdefe9d7abc4e86b
pasojucle <pasojucle@free.fr>
2024-01-02 21:10:04 +0100

    Imposer une tranche d'age à une sortie #207

--- src/UseCase/BikeRide/IsRegistrable.php
+++ src/UseCase/BikeRide/IsRegistrable.php
@@ -6,6 +6,7 @@ namespace App\UseCase\BikeRide;
 
 use App\Entity\BikeRide;
 use App\Entity\BikeRideType;
+use App\Entity\Level;
 use App\Entity\User;
 use DateInterval;
 use DateTime;
@@ -28,12 +29,13 @@ class IsRegistrable
         if (!$users->isEmpty() && !$users->contains($user)) {
             return false;
         }
+        
+        if (!$user->getMemberIdentity()) {
+            return false;
+        }
 
-        if ($bikeRide->getMinAge() && $bikeRide->getMaxAge()) {
-            $memberAge = (int) $bikeRide->getStartAt()->diff($user->getMemberIdentity()->getBirthDate())->format('%Y');
-            if (!$user->getMemberIdentity() || $memberAge < $bikeRide->getMinAge() || $bikeRide->getMaxAge() < $memberAge) {
-                return false;
-            }
+        if ($bikeRide->getMinAge() && $bikeRide->getMaxAge() && !$this->canParticipateByAge($bikeRide, $user)) {
+            return false;
         }
 
         $today = new DateTime();
@@ -44,5 +46,19 @@ class IsRegistrable
         $closingAt = $bikeRide->getStartAt()->setTime(23, 59, 59);
 
         return $displayAt->sub($intervalDisplay) <= $today && $today <= $closingAt->sub($intervalClosing);
+    }
+
+    private function canParticipateByAge(BikeRide $bikeRide, User $user): bool
+    {
+        if ($bikeRide->getBikeRideType()->isNeedFramers() && Level::TYPE_FRAME === $user->getLevel()->getType()) {
+            return true;
+        }
+
+        $memberAge = (int) $bikeRide->getStartAt()->diff($user->getMemberIdentity()->getBirthDate())->format('%Y');
+        if ($memberAge < $bikeRide->getMinAge() || $bikeRide->getMaxAge() < $memberAge) {
+            return false;
+        }
+
+        return true;
     }
 }
