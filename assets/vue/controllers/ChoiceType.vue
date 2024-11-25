698d6c4fc0ea31cc093c778e57e6a994f9f04290
pasojucle <pasojucle@free.fr>
2024-11-25 18:36:05 +0100

    Correction ajout compétences

--- assets/vue/controllers/ChoiceType.vue
+++ assets/vue/controllers/ChoiceType.vue
@@ -1,6 +1,6 @@
 <template>
     <label for="id">{{ label }}</label>
-    <select v-model="selected" class="form-control form-control-sm">
+    <select v-model="selected" class="form-control form-control-sm" :name="name" :id="id">
         <option v-for="entity in store.list[this.className]" :key="entity.id" :value="entity.id">{{ store.toSring(entity) }}</option>
     </select>
 </template>
@@ -12,6 +12,7 @@ import { store } from './store.js'
 export default {
     props: {
         id: String,
+        name: String,
         className: String,
         exclude: String,
         value: String,
