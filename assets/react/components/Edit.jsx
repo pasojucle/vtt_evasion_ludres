f29f4121a3070e444345f4458c015ef7df35fed8
pasojucle <pasojucle@free.fr>
2025-01-14 19:23:55 +0100

    skill list #274

---
+++ assets/react/components/Edit.jsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { checkStatus, isJsonResponse } from './../../js/fetch.js'
+
+export default function Edit({edit, size, route, handleEditChange, update}) {
+
+    const [title, setTitle] = useState('');
+    const [theme, setTheme] = useState(null);
+    const [form, setForm] = useState({});
+    const [loaded, setLoaded] = useState(false);
+    
+    useEffect(() => {
+        
+        
+    }, [])
+
+    const load = () => {
+        console.log('useEffect', edit, route, loaded)
+        if (edit && route && !loaded) {
+            fetch(route)
+                .then(checkStatus)
+                .then(isJsonResponse)
+                .then((response) => response.json())
+                .then((json)=> {
+                    console.log('fetch edit', json)
+                    setForm(json.form);
+                    setTitle(json.title);
+                    setTheme(json.theme);
+                    setLoaded(true);
+                });
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        console.log('submit', form)
+        Array.from(form.elements).forEach((element) => {
+            if ('-1' === element.value) {
+                element.value = null;
+            }
+        })
+        fetch(form.action, {
+            method: 'POST',
+            body : new FormData(form),
+        })
+        .then(checkStatus)
+        .then(isJsonResponse)
+        .then((response) => response.json())
+        .then((json)=> {
+            console.log('response', json)
+            if (json.success) {
+                update(json.data)
+                hide(); 
+            }
+            if (json.form) {
+                setForm(json.form);
+            }
+        });
+    }
+    const hide = () => {
+        handleEditChange(false);
+        setTimeout(() => {
+            setLoaded(false);
+            setForm({});
+        }, 500);
+    }
+    const createMarkup = () => {
+        console.log('form.element', form.elements)
+        return {__html: form.elements};
+    }
+
+    const ModalContent = () => {
+        console.log('loaded', loaded)
+        load();
+        if (loaded) {
+            return (
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <button type="button" className="close" onClick={hide} aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 className="modal-title">{ title }</h4>
+                    </div>
+                    <form action={form.action} onSubmit={(event) => onSubmit(event)}>
+                        <div dangerouslySetInnerHTML={createMarkup(form.elements)} className="modal-body"></div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" onClick={hide}>Annuler</button>
+                            <button type="submit" className={'btn ' + theme}>{ form.submit }</button>
+                        </div>
+                    </form>
+                </div>
+            )
+        }
+    }
+
+    return (<div className={(edit) ? 'modal fade' : 'modal'} tabIndex="-1" role="dialog">           
+        <div className={(edit) ? 'modal-dialog modal-open ' + size : 'modal-dialog '+ size } role="document">
+            <ModalContent />
+        </div>
+    </div>)
+}
