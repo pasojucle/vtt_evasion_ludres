cdf74c9227fef406c6b4fa1530c1040d1a5b97c6
pasojucle <pasojucle@free.fr>
2025-01-26 16:58:30 +0100

    Modification cluster eval #274

---
+++ assets/react/components/TextType.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function TextType({id, name, value, disabled, className}) {
+
+
+
+    return (
+        <div className={className}>
+            <input className="form-control form-control-sm" type="text" id={id}  name={name} value={value} disabled={disabled}/>
+        </div>
+    )
+}
