d30f947fa66daf232b3994ee45147235e174ef07
pasojucle <pasojucle@free.fr>
2025-03-29 08:37:44 +0100

    Correction message commande validée #278

--- migrations/Version20250329061644.php
+++ migrations/Version20250329061644.php
@@ -22,9 +22,10 @@ final class Version20250329061644 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $message = [
             'name' => 'ORDER_ACKNOWLEDGEMENT_MESSAGE',
+            'label' => 'Message à afficher sur une commande validée par le club',
             'content' => '<p>VOTRE COMMANDE EST VALIDÉE.</p><p>ELLE A PEUT ÊTRE ÉTÉ MODIFIÉE EN FONCTION DES STOCKS.</p><p><strong>Document &agrave; imprimer<br /> et &agrave; envoyer accompagn&eacute; de votre r&eacute;glement<br />A l&#39;adresse suivante :<br />VTT Evasion Ludres<br />184 rue Saint Exup&eacute;ry - 54710 Ludres</strong></p>',
         ];
-        $this->addSql('UPDATE message set content = :content WHERE name = :name', $message);
+        $this->addSql('UPDATE message set content=:content, label=:label WHERE name = :name', $message);
 
     }
 
