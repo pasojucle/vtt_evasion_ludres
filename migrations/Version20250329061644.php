f3e0ceeafca522f6957484972f4c3a2f7cc05088
pasojucle <pasojucle@free.fr>
2025-03-29 07:22:17 +0100

    Correction message commande validée #278

---
+++ migrations/Version20250329061644.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250329061644 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $message = [
+            'name' => 'ORDER_ACKNOWLEDGEMENT_MESSAGE',
+            'content' => '<p>VOTRE COMMANDE EST VALIDÉE.</p><p>ELLE A PEUT ÊTRE ÉTÉ MODIFIÉE EN FONCTION DES STOCKS.</p><p><strong>Document &agrave; imprimer<br /> et &agrave; envoyer accompagn&eacute; de votre r&eacute;glement<br />A l&#39;adresse suivante :<br />VTT Evasion Ludres<br />184 rue Saint Exup&eacute;ry - 54710 Ludres</strong></p>',
+        ];
+        $this->addSql('UPDATE message set content = :content WHERE name = :name', $message);
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
