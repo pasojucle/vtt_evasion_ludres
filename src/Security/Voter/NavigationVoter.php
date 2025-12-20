8de89e4544bf1c183de3a2efdfbee44f2e8dbb3a
pasojucle <pasojucle@free.fr>
2025-12-20 18:43:42 +0100

    Correction des deprecations #317

--- src/Security/Voter/NavigationVoter.php
+++ src/Security/Voter/NavigationVoter.php
@@ -2,12 +2,13 @@
 
 namespace App\Security\Voter;
 
-use App\Dto\DtoTransformer\UserDtoTransformer;
-use App\Entity\Enum\PermissionEnum;
 use App\Entity\User;
+use App\Entity\Enum\PermissionEnum;
+use App\Dto\DtoTransformer\UserDtoTransformer;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 final class NavigationVoter extends Voter
 {
@@ -24,7 +25,7 @@ final class NavigationVoter extends Voter
         return $attribute === self::ADMIN;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         /** @var User $user */
         $user = $token->getUser();
