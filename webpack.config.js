2506d34f25a1ac534559f9b4ae5c3f4ebf69e9a7
pasojucle <pasojucle@free.fr>
2026-01-25 11:53:26 +0100

    update symfony/stimulus-bundle #317

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ Encore
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    // enables the Symfony UX Stimulus bridge (used in assets/stimulus_bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
     .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
