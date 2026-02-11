bbe536f50e53fc40aa2091737b263f9a86d8e14f
pasojucle <pasojucle@free.fr>
2026-02-11 19:27:44 +0100

    slideshow stimulus #317

--- assets/admin.js
+++ assets/admin.js
@@ -40,7 +40,5 @@ import './js/line_chart.js';
 import './js/dashboard.js';
 import './js/clusters.js';
 import './js/isGranted.js';
-import './js/slideshow.js';
 import './js/admin_slideshow.js';
-
 import './js/uploadFile.js'
