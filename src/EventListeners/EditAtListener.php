76e3a4abfd753f5885686bce477aa53ef43cbf36
pasojucle <pasojucle@free.fr>
2026-02-20 17:28:47 +0100

    Ne pas actualiser la date de mise à jours au changement de positon #434

--- src/EventListeners/EditAtListener.php
+++ src/EventListeners/EditAtListener.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace App\EventListeners;
 
-use DateTimeImmutable;
-use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
-use Doctrine\ORM\Event\PrePersistEventArgs;
-use Doctrine\ORM\Event\PreUpdateEventArgs;
-use Doctrine\ORM\Events;
 use ReflectionClass;
+use DateTimeImmutable;
+use Doctrine\ORM\Events;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 
 #[AsDoctrineListener(event: Events::prePersist)]
 #[AsDoctrineListener(event: Events::preUpdate)]
@@ -17,19 +18,28 @@ class EditAtListener
 {
     public function prePersist(PrePersistEventArgs $event): void
     {
-        $this->updateDtatime($event);
+        $this->updateDatetime($event);
     }
 
     public function preUpdate(PreUpdateEventArgs $event): void
     {
-        $this->updateDtatime($event);
+        $this->updateDatetime($event);
     }
 
-    private function updateDtatime(PrePersistEventArgs|PreUpdateEventArgs $event): void
+    private function updateDatetime(PrePersistEventArgs|PreUpdateEventArgs $event): void
     {
         $entity = $event->getObject();
         $reflexionClass = new ReflectionClass($entity);
         if ($reflexionClass->hasMethod('setUpdateAt')) {
+            if ($event instanceof PreUpdateEventArgs) {
+                /** @var EntityManagerInterface $objectManager */
+                $objectManager = $event->getObjectManager();
+                $unitOfWork = $objectManager->getUnitOfWork();
+                $changeSet = $unitOfWork->getEntityChangeSet($entity);
+                if (array_key_exists('orderBy', $changeSet)) {
+                    return;
+                }
+            }
             $entity->setUpdateAt(new DateTimeImmutable());
         }
     }
